d6f6f915393a56fb5b77c15c3bb17adb98dee4bf
Jordan Raddick <raddick@jhu.edu>
2014-03-07 16:12:52 -0500

    initial checkin

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<form action="action.php" method="post">
+ <p>Plate number: <input type="text" name="platenumber" /></p>
+ <p><input type="submit" /></p>
+ </form>
+
+</body>
+</html>
